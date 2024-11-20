d5224da4de6f0cb4e1a1ae7a3059b2b49270c2e6
Ronald Pardo <koren.yin@outlook.es>
2024-11-20 14:25:48 -0500

    Hardhat start point, check its repo

--- contracts/RMB.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.8.0;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract RMB is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Chinese Yuan", "RMB") {
-        _mint(msg.sender, initialSupply);
-    }
-}
--- truffle/deploy.sh
+++
@@ -1,3 +0,0 @@
-cd truffle
-
-truffle migrate --reset --network besu
